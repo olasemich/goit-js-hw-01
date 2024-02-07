94aece8f7ab61a9a557f0a171fa23fa07d0cc2a4
olasemich <olasemich@gmail.com>
2024-02-07 20:35:27 +0000

    start

---
+++ js/task-1.js
@@ -0,0 +1,7 @@
+const quantity = 3;
+const pricePerDroid = 3000;
+const totalPrice = quantity * pricePerDroid; 
+function makeTransaction = "You ordered <quantity> droids worth <totalPrice> credits!";
+console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
+
+
