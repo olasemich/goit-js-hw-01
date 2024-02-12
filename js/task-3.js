efd7108a49d4ce85caa3e00734ebe51b6a0a72af
olasemich <olasemich@gmail.com>
2024-02-12 18:26:00 +0000

    ачпача

--- js/task-3.js
+++ js/task-3.js
@@ -1,6 +1,6 @@
 function getElementWidth(content, padding, border) {
    return `${content + 2 * (padding + border)}` 
 };
-console.log(getElementWidth("50", "8", "4")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+console.log(getElementWidth(50, 8, 4)); // 74
+console.log(getElementWidth(60, 12, 8.5)); // 101
+console.log(getElementWidth(200, 0, 0)); // 200
