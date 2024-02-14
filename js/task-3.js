df5ac0d1d44ef609553af413f10b2429dba9294d
olasemich <olasemich@gmail.com>
2024-02-14 20:23:20 +0000

    update task-3

--- js/task-3.js
+++ js/task-3.js
@@ -1,6 +1,10 @@
 function getElementWidth(content, padding, border) {
-   return `${content + 2 * (padding + border)}` 
-};
+  const contentWidth = parseFloat(content);
+  const paddingWidth = parseFloat(padding) * 2;
+  const borderWidth = parseFloat(border) * 2;
+
+  return contentWidth + paddingWidth + borderWidth;
+}
 console.log(getElementWidth(50, 8, 4)); // 74
 console.log(getElementWidth(60, 12, 8.5)); // 101
 console.log(getElementWidth(200, 0, 0)); // 200
