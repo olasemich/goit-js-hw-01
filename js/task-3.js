25bee3f29b43112df7694833762c22ce622439c0
olasemich <olasemich@gmail.com>
2024-03-11 17:30:49 +0000

    xzc

--- js/task-3.js
+++ js/task-3.js
@@ -8,3 +8,13 @@ function getElementWidth(content, padding, border) {
 console.log(getElementWidth(50, 8, 4)); // 74
 console.log(getElementWidth(60, 12, 8.5)); // 101
 console.log(getElementWidth(200, 0, 0)); // 200
+
+function checkFileExtension(fileName, ext) {
+ 
+  if (fileName.endsWith()) {
+    "File extension matches"
+  } else {
+    "File extension does not match"
+  }
+}
+checkFileExtension("styles.css", ".css")
